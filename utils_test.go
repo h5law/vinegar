b565162f9098d1663b578494bcc5450d03a1142a
h5law <dev@h5law.com>
2024-05-15 06:52:12 +0100

    feat: test table keyword formatting and extract helper function

--- utils_test.go
+++ utils_test.go
@@ -10,19 +10,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[](){}~\\/;:"
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[](){}~/;:"
 
-func TestFormatKeyword(t *testing.T) {
+func TestFormatKeyword_Random(t *testing.T) {
 	wg := &sync.WaitGroup{}
 	for i := 0; i < min(runtime.NumCPU(), runtime.GOMAXPROCS(0)); i++ {
 		wg.Add(1)
 		go func(t *testing.T, wg *sync.WaitGroup) {
 			defer wg.Done()
-			b := make([]byte, len(alphabet))
-			for i := range b {
-				b[i] = alphabet[rand.Intn(len(alphabet))]
-			}
-			randString := string(b)
+			randString := randomString(t)
 			str := strings.ReplaceAll(randString, " ", "")
 			str = strings.ToLower(str)
 			for _, c := range str {
@@ -35,4 +31,36 @@ func TestFormatKeyword(t *testing.T) {
 		}(t, wg)
 	}
 	wg.Wait()
+}
+
+func TestFormatTableKeyword_Random(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	for i := 0; i < min(runtime.NumCPU(), runtime.GOMAXPROCS(0)); i++ {
+		wg.Add(1)
+		go func(t *testing.T, wg *sync.WaitGroup) {
+			defer wg.Done()
+			randString := randomString(t)
+			formatTableStr := formatTableKeyword(randString)
+			require.Len(t, formatTableStr, 26)
+			seen := make([]int, 26)
+			for _, c := range formatTableStr {
+				if seen[c%26] != 0 {
+					(*t).Fatalf("[error] found duplicate character: %c", c)
+				} else {
+					seen[c%26] = 1
+
+				}
+			}
+		}(t, wg)
+	}
+	wg.Wait()
+}
+
+func randomString(t *testing.T) string {
+	t.Helper()
+	b := make([]byte, len(alphabet))
+	for i := range b {
+		b[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(b)
 }
